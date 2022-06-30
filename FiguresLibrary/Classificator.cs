38a2ee23c39529e0d9605c4277a4a2d91c4ab6f7
AriesPL <blackhorns1989@gmail.com>
2022-06-30 07:04:48 +0300

    Добавьте файлы проекта.

---
+++ FiguresLibrary/Classificator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FiguresLibrary
+{
+	public static class Classificator
+	{
+		public static double Triangle(double a, double b, double c)
+		{
+			if (a < 0 || b < 0 || c < 0)
+				return 0;
+
+			double p = (a + b + c) / 2;
+
+			double sumArea = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+			return sumArea;
+		}
+
+		public static double Cirlcle(double r)
+		{
+			if (r < 0)
+				return 0;
+			double sumArea = Math.PI * Math.Pow(r, 2);
+
+			return sumArea;
+		}
+	}
+}
---
+++ UnitTestFigure/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestFigure")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestFigure")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a8e4fd5-8e29-45d4-9bf0-202c1783082d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
